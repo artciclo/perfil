c221e023580e1b9ea13cece0de6c9783586522de
artciclo <artciclo@gmail.com>
2024-11-18 17:19:50 -0300

    reset

---
+++ FF-logica/conceitosdados.js
@@ -0,0 +1,11 @@
+let string = "Aqui está nossa string"; // string 
+let numberInt = 363; // number ou numero inteiro
+let numberFloat = 3.5; // numero de ponto flutuante
+ 
+
+let boolTrue = true; // booleano true
+let boolfalse = false; // booleando false
+let estaChovendo = false;
+
+let empty; //undefined ou vazio
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# repositorio de estudos
+
+_Página de estudos e alguns links_
+
+resetanto o repositorio
